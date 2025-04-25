47c1fef6980065164e8e1f6642d229f2bad6e403
Xavi <xpriet0431@rcs-k12.us>
2025-04-25 12:43:56 -0400

    Update Deck.ts

--- src/models/Deck.ts
+++ src/models/Deck.ts
@@ -13,7 +13,7 @@ export class Deck {
     this.cards.length = 0;
     const suits: Suit[] = ['H', 'S', 'C', 'D'];
     suits.forEach((suit) => {
-      this.cards.push(...[new Card(suit, 'A'), new Card(suit, 1), new Card(suit, 2), new Card(suit, 3), new Card(suit, 4), new Card(suit, 5), new Card(suit, 6), new Card(suit, 7), new Card(suit, 8), new Card(suit, 9), new Card(suit, 10), new Card(suit, 'J'), new Card(suit, 'Q'), new Card(suit, 'K')]);
+      this.cards.push(...[new Card(suit, 'A'), new Card(suit, 2), new Card(suit, 3), new Card(suit, 4), new Card(suit, 5), new Card(suit, 6), new Card(suit, 7), new Card(suit, 8), new Card(suit, 9), new Card(suit, 10), new Card(suit, 'J'), new Card(suit, 'Q'), new Card(suit, 'K')]);
     });
     this.shuffle();
     console.log(this.cards);
